942748c77874b70867dfe8f586be3aa51fd2bbfc
Alex <github@alexv525.com>
2020-09-19 14:53:17 +0800

    :recycle: Copy all action files into the docker container. Fix #9

--- Dockerfile
+++ Dockerfile
@@ -1,6 +1,8 @@
 FROM google/dart:2.9
 
 COPY entrypoint.sh /entrypoint.sh
+COPY . /github/action_files/
+
 RUN chmod +x /entrypoint.sh
 
 ENTRYPOINT ["/entrypoint.sh"]
--- entrypoint.sh
+++ entrypoint.sh
@@ -1,5 +1,8 @@
 #!/bin/sh
 
+# Enter the action's path to make sure we run below codes from action.
+cd /github/action_files/ || exit
+
 sh -c "pub get"
 sh -c "dart bin/main.dart $1 $2 -t $3 -p $4 --words='$5'"
 exit 0
